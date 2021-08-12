eae8e7b2e7c2f1a0111a51e9bd7e361fa4fccfee
Mario <marioparrilla2@gmail.com>
2021-08-12 19:45:56 +0200

    Falta dar vuelta 90º

--- src/rubikNumbers/ExpertMode.java
+++ src/rubikNumbers/ExpertMode.java
@@ -75,7 +75,7 @@ public class ExpertMode implements ModeInterface {
         int[][] copyOfBoard = board;
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
-                copyOfBoard[j][board.length-1-i]=board[i][j];
+                //Aqui va el algoritmo
             }
         }
         board=copyOfBoard;
